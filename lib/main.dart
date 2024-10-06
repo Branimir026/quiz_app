e1721b5156f30ca9873c67f88ec5abb686940925
Branimir026 <br.belic@gmail.com>
2024-10-06 23:08:31 +0200

    created start screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,21 @@ import 'package:quiz_app/start_screen.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: StartScreen(),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 86, 21, 199),
+                Color.fromARGB(255, 80, 46, 138),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
       ),
     ),
   );
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -5,6 +5,33 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Start Screen');
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+                color: Color.fromARGB(255, 209, 160, 255), fontSize: 24),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+                foregroundColor: const Color.fromARGB(255, 209, 160, 255)),
+            child: const Text('Start Quiz!'),
+          )
+        ],
+      ),
+    );
   }
 }
