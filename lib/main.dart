c642fe2179a3892b53e17e64a97c16c3739dbdaf
Branimir026 <br.belic@gmail.com>
2024-10-06 21:52:37 +0200

    continuing with course

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,12 @@
 import 'package:flutter/material.dart';
 
+import 'package:quiz_app/start_screen.dart';
+
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 57, 32, 99),
-                Color.fromARGB(255, 101, 58, 177)
-              ],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-            ),
-          ),
-          child: Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Image.asset(
-                  'assets/images/quiz-logo.png',
-                  width: 300,
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
-                const Text(
-                  'Learn Flutter the fun way!',
-                  style: TextStyle(
-                    fontSize: 22,
-                    color: Colors.white,
-                  ),
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                FilledButton(
-                  onPressed: () {},
-                  style: OutlinedButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(255, 57, 32, 99),
-                    foregroundColor: Colors.white,
-                    textStyle: const TextStyle(fontSize: 18),
-                  ),
-                  child: const Text('Start Quiz!'),
-                )
-              ],
-            ),
-          ),
-        ),
+        body: StartScreen(),
       ),
     ),
   );
