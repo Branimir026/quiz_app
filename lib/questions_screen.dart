70b4170f4eae3b14e5ed7035b07dccedd8a78f97
Branimir026 <br.belic@gmail.com>
2024-10-08 22:17:30 +0200

    creating state, and rendering different screens based on state

---
+++ lib/questions_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class QuestionsScreen extends StatefulWidget {
+  const QuestionsScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _QuestionsScreenState();
+  }
+}
+
+class _QuestionsScreenState extends State<QuestionsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'Question screen!',
+          ),
+        ],
+      ),
+    );
+  }
+}
